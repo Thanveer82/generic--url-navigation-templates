813b1b4e74be7a37d809767bec2127505a609f2d
Thanveer82 <parigithanveer@gmail.com>
2023-03-16 12:59:28 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rcb(request):
+    return render(request,'Rcb.html')
+def virat(request):
+    return render(request,'Virat.html')
